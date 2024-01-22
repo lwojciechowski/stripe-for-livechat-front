2c8f29925d4e00cfbb2ec2552325d9fbe1e4df98
Damian Garbacz <dgarbacz@livechat-integrations.com>
2024-01-22 12:47:15 +0100

    fix: Before react update.

--- src/Auth.js
+++ src/Auth.js
@@ -1,43 +1,40 @@
-import React, { useEffect, useState, useRef } from "react";
-import { accountsSdk } from "@livechat/accounts-sdk";
-import { authRef } from "./authRef";
-import Loading from "./Loading";
+import React, { useEffect, useState } from "react";
+import AccountsSDK from "@livechat/accounts-sdk";
 
-const Auth = ({ children, signIn, clientId }) => {
-  const [loading, setLoading] = useState(true);
+const LC_CLIENT_ID = process.env.REACT_APP_LC_CLIENT_ID;
+const sdk = new AccountsSDK({
+  client_id: LC_CLIENT_ID,
+});
+
+const Auth = ({ children }) => {
   const [auth, setAuth] = useState(null);
-  const authInstance = useRef(null);
-
   useEffect(() => {
-    authInstance.current = accountsSdk.init({
-      client_id: clientId,
-      onIdentityFetched: (error, data) => {
-        setLoading(false);
-        if (data) {
-          setAuth(data);
-          authRef.token = data.access_token;
+    sdk
+      .redirect()
+      .authorizeData()
+      .then((data) => {
+        authRef.token = data.access_token;
+        authRef.scopes = data.scope.split(",");
+        setAuth(data);
+      })
+      .catch((e) => {
+        console.error(e);
+        if (e.identity_exception === "unauthorized") {
+          sdk.redirect().authorize();
         }
-        if (error) {
-          console.error(error);
-        }
-      }
-    });
-  }, [clientId]);
-
-  if (loading) {
-    return (
-      <div>
-        <Loading />
-      </div>
-    );
-  }
+      });
+  }, []);
 
   return (
     <AuthContext.Provider value={auth}>
-      {auth !== null ? children : signIn(authInstance)}
+      {auth !== null ? children : null}
     </AuthContext.Provider>
   );
 };
 
 export const AuthContext = React.createContext(null);
 export default Auth;
+export const authRef = {
+  token: null,
+  scopes: [],
+};
